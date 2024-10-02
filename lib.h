d5ba94a5844065127a3b760437534ebda4eb40fc
Kseniya <fku1992@gmail.com>
2024-10-02 23:27:14 +0300

    added allocator realization

--- lib.h
+++ lib.h
@@ -1,4 +1,107 @@
 #pragma once
 
-int version();
+#include <iostream>
+#include <memory>
+struct deleter
+{
+    void operator()(void* ptr)
+    {
+        #ifndef NDEBUG
+            std::cout << "deleter" << std::endl;
+        #endif
 
+        ::operator delete(ptr);
+    }
+};
+
+template <class T, std::size_t Capacity = 100>
+struct MyAllocator
+{
+    using value_type = T;
+
+    std::shared_ptr<void> pool;
+    std::size_t fillSize = 0;
+
+    MyAllocator() noexcept
+    : pool(::operator new(Capacity * sizeof(T)), deleter())
+    , fillSize(0)
+    {}
+
+    template <class U, std::size_t CapacityOther>
+    MyAllocator(const MyAllocator <U, CapacityOther>& a) noexcept
+    : MyAllocator()
+    {
+        if (Capacity == CapacityOther)
+            pool = a.pool;
+    }
+
+    [[nodiscard]] T* allocate(std::size_t n)
+    {
+        if ((n != 1) || (fillSize + n > Capacity))
+            throw std::bad_alloc();
+
+        if (auto p = reinterpret_cast<T*>(reinterpret_cast<uint8_t *>(pool.get()) + fillSize * sizeof(T)))
+        {
+            #ifndef NDEBUG
+                report(p, n);
+            #endif
+
+            fillSize += n;
+            return p;
+        }
+
+        throw std::bad_alloc();
+    }
+
+    void deallocate(T* p, std::size_t n)
+    {
+        // dummy implementation
+        #ifndef NDEBUG
+            report(p, n, 0);
+        #endif
+
+        if (p == nullptr)
+            throw std::invalid_argument("Can't deallocate a null pointer");
+
+        if (fillSize < n)
+            throw std::runtime_error("Can't deallocate already deallocated memory");
+
+        fillSize -= n;
+    }
+
+    MyAllocator select_on_container_copy_construction() const
+    {
+        return MyAllocator();
+    }
+
+    template <class U>
+    struct rebind
+    {
+        typedef MyAllocator<U, Capacity> other;
+    };
+
+    using propagate_on_container_copy_assignment = std::true_type;
+    using propagate_on_container_move_assignment = std::true_type;
+    using propagate_on_container_swap = std::true_type;
+
+private:
+
+    void report(T* p, std::size_t n, bool alloc = true) const
+    {
+        std::cout << (alloc ? "Alloc: " : "Dealloc: ") << sizeof(T) * n
+                  << " bytes at " << std::hex << std::showbase
+                  << reinterpret_cast<void*>(p) << std::dec << '\n';
+    }
+};
+
+template <class T, std::size_t Capacity1, class U, std::size_t Capacity2>
+bool operator==(const MyAllocator <T, Capacity1>& a1, const MyAllocator <U, Capacity2>& a2)
+{
+    return (a1.pool == a2.pool) && (Capacity1 == Capacity2);
+}
+
+template <class T, std::size_t Capacity1, class U, std::size_t Capacity2>
+bool operator!=(const MyAllocator <T, Capacity1>& a1, const MyAllocator <U, Capacity2>& a2)
+{
+    return (a1.pool != a2.pool) || (Capacity1 != Capacity2);
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include "lib.h"
 
 #include <iostream>
+#include <map>
+#include <vector>
 
-int main(int, char **) {
-	std::cout << "Version: " << version() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
-	return 0;
+constexpr int factorial(const int n)
+{
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}
+
+int main(int, char **)
+{
+    std::map<int, int> my_map1;
+    for (std::size_t i = 0; i < 10; i++)
+        my_map1[i] = factorial(i);
+
+    std::map<int, int, std::less<int>, MyAllocator<std::pair<const int, int>, 10>> my_map2;
+    for (std::size_t i = 0; i < 10; i++)
+        my_map2[i] = factorial(i);
+
+    for (auto [key, value] : my_map2)
+        std::cout << key << " " << value << std::endl;
+
+
+/*
+    std::map<int, int, std::less<int>, MyAllocator<std::pair<const int, int>>> my_map {{0, 1},{1, 1},{2, 1}};
+    my_map.clear();
+    my_map[0] = 1;
+    my_map[1] = 1;
+    my_map.erase(1);
+    my_map[1] = 1;
+    my_map[1] = 2;
+*/
+
+    // std::vector<int, MyAllocator<int>> v{1};
+
+    return 0;
 }
