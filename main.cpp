37b17035ac74777aa2a27434304571ccfafaa36d
Kseniya <fku1992@gmail.com>
2025-01-06 18:56:32 +0300

    added handler

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
-#include "lib.h"
+#include "handler.h"
 
 #include <iostream>
 
-int main(int, char **) {
-	std::cout << "Version: " << version() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
-	return 0;
+int main(int argc, char *argv[]) {
+    // if (argc > 1)
+    {
+        std::cout << argc << std::endl; //TODO delete
+        std::cout << argv[0] << std::endl; //TODO delete
+
+        // if (argv[1] < 0)
+            // throw std::runtime_error("Wrong static block size.");
+
+        // Handler handler(argv[1]);
+        Handler handler(3);  //TODO delete
+        handler.write(std::cin);
+    }
+    /*
+    else
+    {
+        std::cout << "no args" << std::endl;
+    }
+    */
+
+    return 0;
 }
---
+++ test_parser.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE test_parser
+
+#include "handler.h"
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_parser)
+
+BOOST_AUTO_TEST_CASE(test_first) {
+    // TODO
+    BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
