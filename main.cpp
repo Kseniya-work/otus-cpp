1f125e16a563d0fee1283a6a731449ed43863ca1
Kseniya <fku1992@gmail.com>
2025-03-03 01:12:53 +0300

    turned on args, fixed compile er.

--- main.cpp
+++ main.cpp
@@ -3,17 +3,16 @@
 #include "include/stream_logger.h"
 #include "include/vector_storage.h"
 
+#include <cstdlib>
 #include <iostream>
 #include <memory>
 
 int main(int argc, char *argv[]) {
-    // if (argc > 1)
+    if (argc > 1)
     {
-        std::cout << argc << std::endl; //TODO delete
-        std::cout << argv[0] << std::endl; //TODO delete
-
-        // if (argv[1] < 0)
-            // throw std::runtime_error("Wrong static block size.");
+        auto N = atoi(argv[1]);
+        if (N < 0)
+            throw std::runtime_error("Wrong static block size.");
 
         using storage_type = VectorStorage;
         using handler_type = Handler<storage_type, Logger<storage_type>>;
@@ -22,16 +21,14 @@ int main(int argc, char *argv[]) {
         auto storage = std::make_shared<storage_type>();
         std::vector<logger_ptr_type> loggers = {std::make_shared<StreamLogger<storage_type>>(),
                                                 std::make_shared<FileLogger<storage_type>>()};
-        std::unique_ptr<handler_type> handler(new handler_type{storage, 2, loggers});
+        std::unique_ptr<handler_type> handler(new handler_type{storage, N, loggers});
 
         handler->read(std::cin);
     }
-    /*
     else
     {
         std::cout << "no args" << std::endl;
     }
-    */
 
     return 0;
 }
